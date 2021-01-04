847421121e8958cb7e4e0251cc2288f003bbdba9
DarkoBunny <darkobunny57@gmail.com>
2021-01-04 07:45:11 +0700

    Fix typo

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -277,7 +277,7 @@ async def type_afk_is_not_true(notafk):
 CMD_HELP.update(
     {
         "afk": ">`.off [Alasan Opsional]`"
-        "\nUsage: Menetapkan Anda sebagai afk./nBalas kepada siapa saja yang memberi tag/PM's "
+        "\nUsage: Menetapkan Anda sebagai afk. /nBalas kepada siapa saja yang memberi tag/PM's "
         "Anda memberi tahu mereka bahwa Anda AFK(reason)."
         "\n\n>`.unoff`"
         "\nUsage: Mematikan Anda dari mode AFK"
